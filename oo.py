ee12d690fb11d1a4bf37673bb77e3619ad1d0783
Hannah Ramadan <hannahr315@gmail.com>
2021-02-17 21:14:18 +0000

    kids game

---
+++ oo.py
@@ -0,0 +1,85 @@
+"""Discussion Questions
+
+1. What are the three main design advantages that object orientation
+   can provide? Explain each concept.
+
+(replace this with your answer)
+
+
+2. What is a class?
+
+(replace this with your answer)
+
+
+3. What is a method?
+
+A methods is a class function. It makes it so that you can reuse this function on other
+instances of your class. 
+
+
+4. What is an instance in object orientation?
+
+An instance is an occurance of a class.
+
+
+5. How is a class attribute different than an instance attribute?
+   Give an example of when you might use each.
+
+   A class attribute is an attribute that you want all instances of that class to have, 
+   while an instance attribute is an attribute only that particualr instance gets. 
+
+   For example, a if you had a class that contained food, a class attribute might be "is edible".
+   An instance of the class Food might be fruit, which might have an attribute "contains seeds".
+
+
+"""
+
+
+"""2. Road Class"""
+
+
+# Replace this with your code
+
+
+"""3. Update Password"""
+
+
+class User:
+    """A user object."""
+
+    def __init__(self, username, password):
+        """Create a user with the given username and password."""
+
+        self.username = username
+        self.password = password
+
+
+"""4. Build a Library"""
+
+
+class Book(object):
+    """A Book object."""
+
+    def __init__(self, title, author):
+        """Create a book with the given title and author."""
+
+        self.title = title
+        self.author = author
+
+
+"""5. Rectangle"""
+
+
+class Rectangle:
+    """A rectangle."""
+
+    def __init__(self, length, width):
+        """Create a rectangle with the given length and width."""
+
+        self.length = float(length)
+        self.width = float(width)
+
+    def calculate_area(self):
+        """Return the area of the rectangle."""
+
+        return self.length * self.width
